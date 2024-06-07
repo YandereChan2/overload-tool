98d84dbc329d30ce3d1ac70a78bfe08b2c1f8024
YandereChan2 <160600198+YandereChan2@users.noreply.github.com>
2024-06-07 12:10:25 +0800

    Add files via upload

--- overload.h
+++ overload.h
@@ -1,59 +1,63 @@
 namespace Yc
 {
-  template<class T>
-struct overload_helper;
-template<class R,class...Args>
-struct overload_helper<R(Args...)>
-{
-	R(*ptr)(Args...);
-	R operator()(std::conditional_t<std::is_lvalue_reference_v<Args>, Args&, std::conditional_t<std::is_rvalue_reference_v<Args>, Args&&, const Args&>>... args)
+	namespace detail
 	{
-		return ptr(std::forward<std::conditional_t<std::is_lvalue_reference_v<Args>, Args&, std::conditional_t<std::is_rvalue_reference_v<Args>, Args&&, const Args&>>>(args)...);
-	}
-};
-template<class R, class...Args>
-struct overload_helper<R(Args......)>
-{
-	R(*ptr)(Args......);
-	template<class...Ex>
-	R operator()(std::conditional_t<std::is_lvalue_reference_v<Args>, Args&, std::conditional_t<std::is_rvalue_reference_v<Args>, Args&&, const Args&>>... args,Ex&&... ex)
-	{
-		return ptr(std::forward<std::conditional_t<std::is_lvalue_reference_v<Args>, Args&, std::conditional_t<std::is_rvalue_reference_v<Args>, Args&&, const Args&>>>(args)...,std::forward<Ex>(ex)...);
-	}
-};
-template<class T>
-struct overload_chooser
-{
-	using type = T;
-};
-template<class R, class...Args>
-struct overload_chooser<R(Args...)>
-{
-	using type = overload_helper<R(Args...)>;
-};
-template<class R, class...Args>
-struct overload_chooser<R(Args......)>
-{
-	using type = overload_helper<R(Args......)>;
-};
-template<class R, class...Args>
-struct overload_chooser<R(*)(Args...)>
-{
-	using type = overload_helper<R(Args...)>;
-};
-template<class R, class...Args>
-struct overload_chooser<R(*)(Args......)>
-{
-	using type = overload_helper<R(Args......)>;
-};
-template<class T>
-using overload_chooser_t = overload_chooser<T>::type;
+		template<class T>
+		struct overload_helper;
+		template<class R, class...Args>
+		struct overload_helper<R(Args...)>
+		{
+			R(*ptr)(Args...);
+			R operator()(std::conditional_t<std::is_lvalue_reference_v<Args>, Args&, std::conditional_t<std::is_rvalue_reference_v<Args>, Args&&, const Args&>>... args)
+			{
+				return ptr(std::forward<std::conditional_t<std::is_lvalue_reference_v<Args>, Args&, std::conditional_t<std::is_rvalue_reference_v<Args>, Args&&, const Args&>>>(args)...);
+			}
+		};
+		template<class R, class...Args>
+		struct overload_helper<R(Args......)>
+		{
+			R(*ptr)(Args......);
+			template<class...Ex>
+			R operator()(std::conditional_t<std::is_lvalue_reference_v<Args>, Args&, std::conditional_t<std::is_rvalue_reference_v<Args>, Args&&, const Args&>>... args, Ex&&... ex)
+			{
+				return ptr(std::forward<std::conditional_t<std::is_lvalue_reference_v<Args>, Args&, std::conditional_t<std::is_rvalue_reference_v<Args>, Args&&, const Args&>>>(args)..., std::forward<Ex>(ex)...);
+			}
+		};
+		template<class T>
+		struct overload_chooser
+		{
+			using type = T;
+		};
+		template<class R, class...Args>
+		struct overload_chooser<R(Args...)>
+		{
+			using type = overload_helper<R(Args...)>;
+		};
+		template<class R, class...Args>
+		struct overload_chooser<R(Args......)>
+		{
+			using type = overload_helper<R(Args......)>;
+		};
+		template<class R, class...Args>
+		struct overload_chooser<R(*)(Args...)>
+		{
+			using type = overload_helper<R(Args...)>;
+		};
+		template<class R, class...Args>
+		struct overload_chooser<R(*)(Args......)>
+		{
+			using type = overload_helper<R(Args......)>;
+		};
+		template<class T>
+		using overload_chooser_t = overload_chooser<T>::type;
 
+	}
+ 
 template<class...Args>
-struct overload :overload_chooser_t<Args>...
+struct overload :detail::overload_chooser_t<Args>...
 {
-	using overload_chooser_t<Args>:: operator() ...;
-	overload(const Args&...args) :overload_chooser_t<Args>{ args }...
+	using detail::overload_chooser_t<Args>:: operator() ...;
+	overload(const Args&...args) :detail::overload_chooser_t<Args>{ args }...
 	{
 	}
 };
